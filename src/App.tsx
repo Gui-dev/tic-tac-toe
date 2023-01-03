8c9ad1e5bab03badfe9886e99487f70df6bb1a73
Gui-dev <adrianon10@gmail.com>
2023-01-03 17:40:00 -0300

    feat: add helper function checkGameEnded and add new property to winningCombinations

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,35 @@ import { GlobalStyle } from "./styles/global"
 function App () {
   const winningCombinations = [
     //Horizontals
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-
+    { indexes: [0, 1, 2], orientation: 'horizontal' },
+    { indexes: [3, 4, 5], orientation: 'horizontal' },
+    { indexes: [6, 7, 8], orientation: 'horizontal' },
     //Verticals
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-
+    { indexes: [0, 3, 6], orientation: 'vertical' },
+    { indexes: [1, 4, 7], orientation: 'vertical' },
+    { indexes: [2, 5, 8], orientation: 'vertical' },
     //Diagonals
-    [0, 4, 8],
-    [2, 4, 6]
+    { indexes: [0, 4, 8], orientation: 'diagonal-1' },
+    { indexes: [2, 4, 6], orientation: 'diagonal-2' }
   ]
   const [gameData, setGameData] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0])
   const [play, setPlay] = useState(1)
-  const [playWinning, setPlayWinning] = useState(null)
+  const [winningCombo, setWinningCombo] = useState<{ indexes: number[], orientation: string } | null>(null)
 
   useEffect(() => {
     checkWinner()
+    checkGameEnded()
   }, [gameData])
+
+  useEffect(() => {
+    if (winningCombo) {
+      alert('O jogo teve um vencendor')
+    }
+  }, [winningCombo])
 
   const handleClickOnBoard = (indexValue: number) => {
     if (gameData[indexValue] !== 0) return
+    if (winningCombo) return
     setGameData(prev => {
       const newGameData = [...gameData]
       newGameData[indexValue] = play
@@ -37,26 +43,38 @@ function App () {
     setPlay(prev => prev === 1 ? 2 : 1)
   }
 
-  const checkWinner = async () => {
+  const checkWinner = () => {
     let winner = null
     for (let values of winningCombinations) {
       if (
-        gameData[values[0]] === 1 &&
-        gameData[values[1]] === 1 &&
-        gameData[values[2]] === 1
+        gameData[values.indexes[0]] === 1 &&
+        gameData[values.indexes[1]] === 1 &&
+        gameData[values.indexes[2]] === 1
       ) {
         winner = 'play 1 venceu'
       }
 
       if (
-        gameData[values[0]] === 2 &&
-        gameData[values[1]] === 2 &&
-        gameData[values[2]] === 2
+        gameData[values.indexes[0]] === 2 &&
+        gameData[values.indexes[1]] === 2 &&
+        gameData[values.indexes[2]] === 2
       ) {
         winner = 'play 2 venceu'
       }
+
+      if (winner) {
+        setWinningCombo(values)
+        break;
+      }
     }
   }
+
+  const checkGameEnded = () => {
+    if (gameData.every(item => item !== 0)) {
+      alert('O jogo não teve um vencendor, deu velha')
+    }
+  }
+
 
   return (
     <>
@@ -66,8 +84,16 @@ function App () {
         <div className="board-game">
           {gameData.map((value, index) => {
             return (
-              <span key={String(index)} onClick={() => handleClickOnBoard(index)}>
-                {value === 0 ? '' : value === 1 ? 'X' : 'O'}
+              <span
+                className={
+                  `${winningCombo?.indexes.includes(index) && winningCombo.orientation}`
+                }
+                key={String(index)}
+                onClick={() => handleClickOnBoard(index)}
+              >
+                {value === 0 && ''}
+                {value === 1 && 'X'}
+                {value === 2 && 'O'}
               </span>
             )
           })}
